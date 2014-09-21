097cea77f34fc7fab0cb6fcee14929ccfe7cdbc1
Chuankai Lin <chuankai@kai.idv.tw>
2014-09-21 14:36:33 +0800

    power and value sysfs entries

--- hx711.c
+++ hx711.c
@@ -5,33 +5,39 @@
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/of_gpio.h>
+#include <linux/sysfs.h>
 
-
-static int power, value;
+static int power_data, value_data;
 static unsigned int dout_pin, pd_sck_pin;
 
-#if 0
-static ssize_t power_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t power_show(struct device_driver *drv, char *buf)
 {
-        return sprintf(buf, "%d\n", foo);
+        return sprintf(buf, "%d\n", power_data);
 }
 
-static ssize_t power_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+static ssize_t power_store(struct device_driver *drv, const char *buf, size_t count)
 {
-        sscanf(buf, "%du", &foo);
+        sscanf(buf, "%d", &power_data);
         return count;
 }
 
-static struct kobj_attribute power_attr = __ATTR_RW(power);
+static DRIVER_ATTR_RW(power);	//struct driver_attribute driver_attr_power
 
-ATTRIBUTE_GROUPS(hx711);
+static ssize_t value_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+        return sprintf(buf, "%d\n", value_data);
+}
 
-static struct attribute_group attr_group = {
-	.attrs = attrs,
+static DRIVER_ATTR_RO(value);
+
+static struct attribute *hx711_attrs[] = {
+	&driver_attr_power.attr,
+	&driver_attr_value.attr,
+	NULL
 };
 
-static struct kobject *kobj;
-#endif
+ATTRIBUTE_GROUPS(hx711);	//contruct attribute_groups *hx711_groups[]
+
 
 static int of_get_gpio_pins(struct device_node *np, unsigned int *_dout_pin, unsigned int *_pd_sck_pin)
 {
@@ -54,16 +60,18 @@ static int hx711_probe(struct platform_device *pdev)
 	int ret;
 
 	ret = of_get_gpio_pins(pdev->dev.of_node, &dout_pin, &pd_sck_pin);
-	if (ret)
-		return ret;
-	
-	ret = gpio_request_one(dout_pin, GPIOF_DIR_IN, "hx711_data") || gpio_request_one(pd_sck_pin, GPIOF_DIR_IN, "hx711_clk");
-	if (ret) {
-		printk(KERN_INFO "GPIO request failed\n");
-		gpio_free(dout_pin);
-		gpio_free(pd_sck_pin);
-		return -EINVAL;
+	if (!ret) {
+		ret = gpio_request_one(dout_pin, GPIOF_DIR_IN, "hx711_data") || gpio_request_one(pd_sck_pin, GPIOF_DIR_IN, "hx711_clk");
+		if (ret) {
+			printk(KERN_INFO "GPIO request failed\n");
+			gpio_free(dout_pin);
+			gpio_free(pd_sck_pin);
+			
+			return -EINVAL;
+		}
 	}
+	
+	return ret;
 }
 
 static int hx711_remove(struct platform_device *pdev)
@@ -86,6 +94,7 @@ static struct platform_driver hx711_driver = {
 		.name = "hx711",
 		.owner = THIS_MODULE,
 		.of_match_table = of_match_ptr(hx711_dt_ids),
+		.groups = hx711_groups,
 	},
 	.probe = hx711_probe,
 	.remove = hx711_remove,
@@ -93,7 +102,13 @@ static struct platform_driver hx711_driver = {
 
 static int __init mod_init(void)
 {
+	int ret;
+
 	printk(KERN_INFO "hx711 module being loaded\n");
+	ret = platform_driver_register(&hx711_driver);
+	if (ret)
+		printk(KERN_INFO "hx711 driver registration failed\n");
+
 
 #if 0
 	kobj = kobject_create_and_add("hx711", kernel_kobj);
@@ -103,12 +118,14 @@ static int __init mod_init(void)
 	}
 #endif
 
-	return 0;
+	return ret;
 }  
 
 static void __exit mod_exit(void) 
 {
 	printk(KERN_INFO "hx7111 module being unloaded\n"); 
+
+	//platform_driver_unregister(&hx711_driver);	TODO:result in kernel panic
 }  
 
 module_init(mod_init);
